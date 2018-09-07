f9cca501ee13781c89ff2a30226f29618917a1ee
Julia Balliet <julia.m.balliet@gmail.com>
2018-09-07 15:55:13 -0500

    set up routes in app.js

---
+++ TODO.md
@@ -0,0 +1,12 @@
+#TO DO:
+
+###Setup
+- [x] set up database
+- [x] set up redux
+- [ ] create views
+    - feeling
+    - content
+    - support
+    - comment
+    - thank you
+
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import Header from '../Header/Header.js';
+import FeelingPage from '../FeelingPage/FeelingPage.js';
+import ContentPage from '../ContentPage/ContentPage.js';
+import SupportPage from '../SupportPage/SupportPage.js';
+import CommentPage from '../CommentPage/CommentPage.js';
+import ThankYouPage from '../ThankYouPage/ThankYouPage.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
-        <br/>
+        <Header />
+        <Route exact path="/" component={FeelingPage} />
+        <Route path="/2" component={ContentPage} />
+        <Route path="/3" component={SupportPage} />
+        <Route path="/4" component={CommentPage} />
+        <Route path="/5" component={ThankYouPage} />
       </div>
     );
   }
