f08e6a9698f219b87b1266c1783eacc422665c1b
Julia Balliet <julia.m.balliet@gmail.com>
2018-09-07 15:47:24 -0500

    setup redux in index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
+import logger from 'logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const feedback = (state = {}, action) => {
+    return state;
+}
+
+const storeInstance = creatStore(
+    combineReducers({feedback}),
+    applyMiddleware(logger),
+);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, 
+    document.getElementById('root'));
 registerServiceWorker();
