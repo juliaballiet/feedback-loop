fdc5ec995bbb1b8294c36c53b94a913ad3bacfc8
Julia Balliet <julia.m.balliet@gmail.com>
2018-09-07 16:16:57 -0500

    set up basic feeling page

--- src/components/FeelingPage/FeelingPage.js
+++ src/components/FeelingPage/FeelingPage.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class FeelingPage extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            rating: 0,
+        }
+    }
+
+    handleInputChange = (event) => {
+        this.setState({
+            rating: event.target.value,
+        });
+    }
+
+    handleNextClick = (event) => {
+        let action = {
+            type: 'ADD_FEEDBACK',
+            payload: this.state
+        }
+
+        this.props.dispatch(action);
+    }
+
     render(){
         return(
             <div>
-
+                <h3>page 1 of 4</h3>
+                <div>
+                    <p>How are you feeling today?</p>
+                    <br /><input onChange={this.handleInputChange}/>
+                    <br /><button onClick={this.handleNextClick}>Next</button>
+                </div>
             </div>
         );
     }
 }
 
-export default FeelingPage;
+export default connect()(FeelingPage);
